a9ebea0b6c0dc506762f76327037d924da540404
sarthak-tyagi123 <sarthaktyagi225@gmail.com>
2025-05-15 13:15:45 +0530

    first commit

---
+++ .history/multer_20250513234857.js
@@ -0,0 +1,12 @@
+const multer=require("multer")
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/images')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    cb(null, file.fieldname + '-' + uniqueSuffix +Path2D.extname(file.originl))
+  }
+})
+
+const upload = multer({ storage: storage })
